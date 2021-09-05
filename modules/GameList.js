c7fd44cb29cdfb2048dca4856e89af5e2fa61b94
Ayrat <gimranov45@gmail.com>
2021-09-05 05:03:39 +0300

    added auth0 to all

--- modules/GameList.js
+++ modules/GameList.js
@@ -5,27 +5,27 @@ const getKey = require('./getKey.js');
 const GameModel = require('../models/GamesModel.js');
 
 function GameList(req, res) {
-  // const token = req.headers.authorization.split(' ')[1];
+  const token = req.headers.authorization.split(' ')[1];
 
   try {
 
-    // jwt.verify(token, getKey, {}, function (err, user) {
-      // if (err) {
-      //   response.status(500).send('invalid token');
-      // }
+    jwt.verify(token, getKey, {}, async function (err, user) {
+      if (err) {
+        response.status(500).send('invalid token');
+      }
 
-      // let email = request.query.email;
-      // if (email === user.email) {
+      let email = req.query.email;
+      if (email === user.email) {
 
-      GameModel.find((err, gamesdb) => {
-        res.status(200).send(gamesdb)
-      })
+        await GameModel.find({ email }, (err, gamesdb) => {
+          res.status(200).send(gamesdb)
+        })
 
-      // } else {
-      //   response.send('authorization failed');
-      // }
+      } else {
+        response.send('authorization failed');
+      }
 
-    // });
+    });
   }
   catch (err) {
     res.status(500).send(`The Database encountered and error: ${err}`);
